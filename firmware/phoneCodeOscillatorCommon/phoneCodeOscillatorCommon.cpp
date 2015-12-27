faa317f27da015288eaac7b77913076647a25cfb
ROBERT LUTSEY <robie@Bender.local>
2015-12-26 19:49:06 -0500

    factored out the rest of the common code to pcocommon

--- firmware/phoneCodeOscillatorCommon/phoneCodeOscillatorCommon.cpp
+++ firmware/phoneCodeOscillatorCommon/phoneCodeOscillatorCommon.cpp
@@ -7,10 +7,33 @@ MIT license.
 #include "Arduino.h"
 #include "phoneCodeOscillatorCommon.h"
 
-PCOCommon::PCOCommon(int ditPin, int dahPin)
+PCOCommon::PCOCommon(int ditPin, int dahPin, int speakerPin)
   {
     _speedPin = ditPin;
+    _ditPin = ditPin;
     _tonePin = dahPin;
+    _dahPin = dahPin;
+    _speakerPin = speakerPin;
+      // CW characteristics
+    int sideToneFreq = 622; // should be a pleasant tone in the range CW ops are used to
+    int wpm = 20;         // operation word per minute
+      // I don't recommend changing anything below here.
+      // unless you are looking for some Farnsworth action. Then you could
+      // play with interLetterLength and interWordLength to achieve that effect.
+    int ditLength = 1200 / wpm;   // this is the length of a dit in milliseconds at desired WPM
+    int dahLength = 3 * ditLength;
+    int interToneLength = ditLength;
+    int interLetterLength = dahLength;
+    int interWordLength = 7 * ditLength;
+    boolean iambic = false; // Don't use this yet. There is a timing issue to figure out
+    // Play() setup
+    // map dit to TRUE and dah to FALSE for use in play
+    boolean dit = true;
+    boolean dah = false;
+    boolean last;
+    unsigned long now;
+    unsigned long interToneLockTimer;
+
   }
 
 void PCOCommon::_checkIfConfigure(int tonePin, int speedPin) {
@@ -22,6 +45,42 @@ void PCOCommon::_checkIfConfigure(int tonePin, int speedPin) {
     _changeCodeSpeed(true);
   }
 }
+
+void PCOCommon::playDit() {
+  now = millis();
+  if (now > interToneLockTimer) {
+    tone(_speakerPin, sideToneFreq, ditLength);
+  }
+  interToneLockTimer = millis() + interToneLength;
+  last = dit;
+}
+
+void PCOCommon::playDah() {
+  now = millis();
+  if (now > interToneLockTimer) {
+    tone(_speakerPin, sideToneFreq, dahLength);
+  }
+  interToneLockTimer = millis() + interToneLength;
+  last = dah;
+}
+
+void PCOCommon::play(boolean sym) {
+  if (sym) {
+    playDit();
+  } else {
+    playDah();
+  } 
+}
+
+void PCOCommon::diDah() {
+  if (digitalRead(_ditPin)==0 && digitalRead(_dahPin)==0) {
+    play(!last);
+    pause();  // FIXME. How are you going to handle the timing
+              // between this pause and the lock in play()?
+  }
+}
+
+void PCOCommon::pause() {}
 
 void PCOCommon::checkIfConfigure() {
   _checkIfConfigure(_tonePin, _speedPin);
--- firmware/phoneCodeOscillatorCommon/phoneCodeOscillatorCommon.h
+++ firmware/phoneCodeOscillatorCommon/phoneCodeOscillatorCommon.h
@@ -12,12 +12,34 @@ MIT license.
 class PCOCommon 
 {
   public: 
-    PCOCommon(int ditPin, int dahPin);
+    PCOCommon(int ditPin, int dahPin, int speakerPin);
     void checkIfConfigure();
+    int sideToneFreq;
+    int wpm;
+    int ditLength;
+    int dahLength;
+    int interToneLength;
+    int interLetterLength;
+    int interWordLength;
+    boolean iambic;
+    boolean dit;
+    boolean dah;
+    boolean last;
+    unsigned long now;
+    unsigned long interToneLockTimer;
+    void playDit();
+    void playDah();
+    void play(boolean sym);
+    void pause();
+    void diDah();
+
   
   private: 
+    int _ditPin;
+    int _dahPin;
     int _speedPin;
     int _tonePin;
+    int _speakerPin;
     void _checkIfConfigure(int tonePin, int speedPin);
     void _changeSideTone(boolean direction);
     void _changeCodeSpeed(boolean direction);
