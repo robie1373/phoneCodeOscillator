a18875028bc2de1a8a27076b5b05b4de523cad01
ROBERT LUTSEY <robie@Bender.local>
2015-12-26 20:17:49 -0500

    uno cleared of shared functions but the ISR is pukin on the playDit and playDah functions from common. I don't feel like dealing with this right now so uno is officially broken

--- firmware/firmware_uno/firmware_uno.ino
+++ firmware/firmware_uno/firmware_uno.ino
@@ -7,64 +7,7 @@ int ditPin = 2; // Uno 2 has hardware interrupt
 int dahPin = 3; // Uno 3 has hardware interrupt
 int speakerPin = 5; // Digital pin 5 on uno is basic PWM
 
-  // CW characteristics
-int sideToneFreq = 622; // should be a pleasant tone in the range CW ops are used to
-int wpm = 20;         // operation word per minute
-  // I don't recommend changing anything below here.
-  // unless you are looking for some Farnsworth action. Then you could
-  // play with interLetterLength and interWordLength to achieve that effect.
-int ditLength = 1200 / wpm;   // this is the length of a dit in milliseconds at desired WPM
-int dahLength = 3 * ditLength;
-int interToneLength = ditLength;
-int interLetterLength = dahLength;
-int interWordLength = 7 * ditLength;
-boolean iambic = false; // Don't use this yet. There is a timing issue to figure out
-
-  // Play() setup
-  // map dit to TRUE and dah to FALSE for use in play
-boolean dit = true;
-boolean dah = false;
-boolean last;
-unsigned long now;
-unsigned long interToneLockTimer;
-
-void playDit() {
-  now = millis();
-  if (now > interToneLockTimer) {
-    tone(speakerPin, sideToneFreq, ditLength);
-  }
-  interToneLockTimer = millis() + interToneLength;
-  last = dit;
-}
-
-void playDah() {
-  now = millis();
-  if (now > interToneLockTimer) {
-    tone(speakerPin, sideToneFreq, dahLength);
-  }
-  interToneLockTimer = millis() + interToneLength;
-  last = dah;
-}
-
-void play(boolean sym) {
-  if (sym) {
-    playDit();
-  } else {
-    playDah();
-  } 
-}
-
-void pause() {}
-
-void diDah() {
-  if (digitalRead(ditPin)==0 && digitalRead(dahPin)==0) {
-    play(!last);
-    pause();  // FIXME. How are you going to handle the timing
-              // between this pause and the lock in play()?
-  }
-}
-
-PCOCommon pcoCommon(ditPin, dahPin);
+PCOCommon pcoCommon(ditPin, dahPin, speakerPin);
 
 void setup() 
 { 
@@ -73,10 +16,10 @@ void setup()
   pcoCommon.checkIfConfigure();
 
   // set up ditPin to read ditButton on regular Arduinos with 2 hardware interrups
-  attachInterrupt(0, playDit, LOW);
+  attachInterrupt(0, pcoCommon.playDit, LOW);
   
   // set up dahPin to read dahButton on regular Arduinos with 2 hardware interrups
-  attachInterrupt(1, playDah, LOW);
+  attachInterrupt(1, pcoCommon.playDah, LOW);
 } 
 
 void loop()
